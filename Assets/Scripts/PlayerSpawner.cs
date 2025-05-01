8ec7c0a3673cf61162a8df4cd72719cda3aa47b0
Dhyego <dfam92@hotmail.com>
2025-05-01 11:33:58 -0300

    Add blocks

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
     public GameObject PlayerPrefab;
+    public Vector3 playerPos;
 
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            // Defina a posição inicial de spawn
-            Vector3 spawnPosition = new Vector3(0, 1, 0);
+            Vector3 spawnPosition = playerPos;
 
-            // Faça o spawn do player com autoridade atribuída ao PlayerRef
             NetworkObject playerNetworkObj = Runner.Spawn(
                 PlayerPrefab,
                 spawnPosition,
@@ -20,12 +19,9 @@ public class PlayerSpawner : SimulationBehaviour, IPlayerJoined
                 player
             );
 
-            // Associa o objeto do jogador ao PlayerRef (necessário para GetPlayerObject funcionar)
             Runner.SetPlayerObject(player, playerNetworkObj);
 
             Debug.Log($"Spawned and registered player object for {player}");
         }
-            
-
     }
 }
